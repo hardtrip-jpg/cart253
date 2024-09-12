75afe22220987bc87d813d35dadaa3cc0ffb1154
hardtrip <user@hardtrip.ca>
2024-09-12 09:27:48 -0400

    function calls

---
+++ topics/function-calls/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Function Calls
+ * Jeremy
+ * 
+ * This program is just used to 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(640, 480);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
+}
