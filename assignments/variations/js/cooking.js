04308a2847ae900474afac2ffc0efa2dacaf93a8
hardtrip <user@hardtrip.ca>
2024-12-04 11:26:29 -0500

    added more cooking actions // REMOVED COOKING ELEMENTS
    cooking is now a lot simpler

--- assignments/variations/js/cooking.js
+++ assignments/variations/js/cooking.js
@@ -47,16 +47,16 @@ let cookingTerminal = new Terminal(
             case 'use':
                 if (second_word === playerCurrentItem){
                     eval(second_word + '.use(' + third_word + ')');
-                }else if(second_word === 'sink' || second_word === 'fridge' || second_word === 'stove'){
+                }else if(second_word === 'sink' || second_word === 'stove'){
                     eval(second_word + '.use(' + third_word + ')');
                 }else{
-                    cookingTerminal.print("You can't use " + second_word.toUpperCase());
+                    cookingTerminal.print("You aren't holding a " + second_word.toUpperCase());
                 }
                 
                 break;
             case 'grab':
                 if (playerCurrentItem === ""){
-                    if (second_word === 'sink' || second_word === 'fridge' || second_word === 'stove' || second_word === 'counter'){
+                    if (second_word === 'sink' || second_word === 'stove' || second_word === 'counter'){
                         cookingTerminal.print("You can't pick up the " + second_word.toUpperCase());
                         return;
                     }
@@ -102,7 +102,7 @@ let cookingTerminal = new Terminal(
     }
 );
 
-let all_words = ["sink", "counter", "box", "stove", "fridge", "pot", "spoon", "milk", "butter", "sausage", "pan", "spatula", "knife", "package"]
+let all_words = ["sink", "counter", "box", "stove", "pot", "spoon", "package"]
 
 let playerCurrentItem = "";
 
@@ -117,23 +117,22 @@ class cookingPlace {
     }
 
     reset(){
-        this.active_state = states[0];
+        this.place_state = states[0];
     }
 
 }
 
 class cookingItem {
-    constructor(name, states, look, place, use) {
+    constructor(name, states, look, use) {
         this.name = name;
         this.states = states;
         this.look = look;
-        this.place = place;
         this.use = use;
         this.item_state = states[0];
     }
 
     reset(){
-        this.active_state = states[0];
+        this.item_state = states[0];
     }
 
 }
@@ -147,10 +146,10 @@ let sink = new cookingPlace([], ['off', 'on'],
     (word) => {
         if (sink.place_state === 'off'){
             cookingTerminal.print("The sink begins to shoot water");
-            sink.place_state = 'on';
+            sink.place_state = sink.states[1];
         }else{
             cookingTerminal.print("The sink fizzles one last drop");
-            sink.place_state = 'off';
+            sink.place_state = sink.states[0];
         }
     },
 
@@ -182,27 +181,6 @@ let stove = new cookingPlace([], ['off', 'on'],
 
 );
 
-let fridge = new cookingPlace([], ['closed', 'open'],
-    () => {
-        if (fridge.place_state === 'open'){
-            placeLooks('FRIDGE', fridge);
-        }
-        else{
-            cookingTerminal.print("The fridge is closed. You can't see what's inside");
-        }
-        
-    },
-    (word) => {
-        if (fridge.place_state === 'closed'){
-            cookingTerminal.print("The fridge lets a chilling freeze escape");
-            fridge.place_state = 'open';
-        }else{
-            cookingTerminal.print("The door emits a loud band when shut");
-            fridge.place_state = 'closed';
-        }
-
-    },
-);
 
 
 /**
@@ -212,116 +190,109 @@ let box = new cookingItem("BOX", ["unopen", "open", "empty"],
     (word) => {
 
     },
-    (secondWord, thirdWord) => {
+    (word) => {
+        console.log(word);
+
+        if (box.item_state === "unopen"){
+            cookingTerminal.print("You open the KD box");
+            box.item_state = 'open'
+        }
+        else if (box.item_state === "open"){
+            if (word === pot){
+                cookingTerminal.print("The empty the box into the POT. The cheese PACKAGE is set aside");
+                if (pot.item_state === 'empty'){
+                    pot.item_state = "filled and dry";
+                }else{
+                    pot.item_state = "filled";
+                }
+                box.item_state = 'empty'
+                return;
+            }
+            cookingTerminal.print("You can't empty the box here");
+        }else{
+            cookingTerminal.print("The box is already empty");
+        }
+
+    },
+
+)
+
+let currentPotIngrediants = [];
+
+let pot = new cookingItem("POT", ["empty", "filled and dry", "wet and empty", "filled", "boiling", "done", "has_ingredients"],
+    (word) => {
 
     },
     (word) => {
+        console.log(sink.place_state);
+        if (word === sink && sink.place_state === 'on'){
+            if (pot.item_state === "filled and dry"){
+                pot.item_state = "filled";
+            }else{
+                pot.item_state = "wet and empty";
+            }
+            cookingTerminal.print("The POT gets filled with cold running water");
+            return;
+        }else if(word === sink && sink.place_state === 'off'){
+            cookingTerminal.print("The POT gets nothing since the sink is not on");
+            return;
+        }
+        cookingTerminal.print("The POT can't be used like this");
+    },
+)
+
+let spoon = new cookingItem("SPOON", ["null"],
+    (word) => {
+
+    },
+    (word) => {
+        if (word === pot ){
+            if (pot.item_state === "boiling"){
+                cookingTerminal.print("You stir the POT, seperating the noodles");
+            }else if (pot.item_state === "has_ingredients"){
+                cookingTerminal.print("You WIN");
+            }else{cookingTerminal.print("You stir the POT, but it's kind of pointless right now");}  
+        }   
+        else{
+            cookingTerminal.print("You bang the SPOON to a distinct 3/4 polyrythym");
+        }
 
     },
 )
 
-let pot = new cookingItem("POT", ["empty", "filled and dry", "wet and empty", "boiling", "done", "has_ingrediants"],
-    (word) => {
 
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let spoon = new cookingItem("SPOON", ["out", "in"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let milk = new cookingItem("MILK", ["cool", "warm"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let butter = new cookingItem("BUTTER", ["cool", "warm"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let sausage = new cookingItem("SAUSAGE", ["uncooked", "cooked", "burnt"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let pan = new cookingItem("PAN", ["empty", "has_stuff", "cooking", "cooked", "cut"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-
-let spatula = new cookingItem("SPATULA", ["null"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
-let knife = new cookingItem("KNIFE", ["null"],
-    (word) => {
-
-    },
-    (secondWord, thirdWord) => {
-
-    },
-    (word) => {
-
-    },
-)
 let package = new cookingItem("PACKAGE", ["unopen", "open", "empty"],
     (word) => {
 
     },
-    (secondWord, thirdWord) => {
-
-    },
     (word) => {
+        if (word === pot){
+            switch (package.item_state){
+                case 'unopen':
+                    cookingTerminal.print("The PACKAGE isn't open");
+                case 'open':
+                    cookingTerminal.print("You pour the PACKAGE into the POT");
+                    package.item_state = 'empty';
+                case 'empty':
+                    cookingTerminal.print("The PACKAGE is already empty.");
+            }
+
+            
+        }else if(word){
+            cookingTerminal.print("You can't use the PACKAGE like this");
+        }else{
+            switch (package.item_state){
+                case 'unopen':
+                    cookingTerminal.print("You open the PACKAGE");
+                    package.item_state = 'open';
+                case 'open':
+                    cookingTerminal.print("The PACKAGE is already open. It might be used in something else now");
+                case 'empty':
+                    cookingTerminal.print("The PACKAGE is already empty.");
+            }
+            
+
+        }
 
     },
 )
@@ -360,6 +331,7 @@ function cookingReset() {
     currentAngle = 0;
 
     counter.items = [box, knife, spatula,];
+    stove.items = [pot, pan, ]
 }
 
 function placeLooks(name, place) {
